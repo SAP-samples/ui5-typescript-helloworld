7979627ab3bbb62b1a47b026a1e7af1fbb597ca2
akudev <andreas.kunz@sap.com>
2026-06-10 12:40:05 +0200

    Update to UI5 1.148 and TypeScript 6 + deps; switch to @openui5/types
    Also update to manifest version 2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,8 @@
-import eslint from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-	eslint.configs.recommended,
+	tseslint.configs.eslintRecommended,
 	...tseslint.configs.recommended,
 	...tseslint.configs.recommendedTypeChecked,
 	{
